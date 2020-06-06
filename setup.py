ba7905314f1ba4e9284f38b0882f537d5dabae73
Jarry Shaw <jarryshaw@icloud.com>
2020-06-06 17:26:18 +0800

    New distribution [0.8.4.post1]
     * missing out "\\x0c" control char

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md', 'rb') as file:
     long_desc = file.read().decode('utf-8')
 
 # version string
-__version__ = '0.8.4'
+__version__ = '0.8.4.post1'
 
 # set-up script for pip distribution
 setup(
