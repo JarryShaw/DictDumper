4e8984c10fba86ea3d0db7bf0c05c42ce47ebb50
Jarry Shaw <jarryshaw@icloud.com>
2020-06-06 17:28:32 +0800

    New distribution [0.8.4.post2]
     * missing out "\\x0b" control char

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Jarry Shaw'
 author = 'Jarry Shaw'
 
 # The full version, including alpha/beta/rc tags
-release = '0.8.4.post1'
+release = '0.8.4.post2'
 
 
 # -- General configuration ---------------------------------------------------
